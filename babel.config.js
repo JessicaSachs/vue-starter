3e30b0c41cc763694c3fc4a8202372833d71f75e
Jess <jessica.sachs>
2019-02-07 12:52:24 -0500

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+    presets: [
+        '@vue/app'
+    ],
+    plugins: [
+        ['@babel/plugin-transform-runtime', {
+            regenerator: true,
+            ignore: ['src/javascripts/pop/**/*.es5.js', 'node_modules/@babel/runtime'],
+        }],
+        ['@babel/plugin-syntax-dynamic-import'],
+        ['@babel/plugin-transform-shorthand-properties'],
+        ['@babel/plugin-proposal-decorators', {legacy: true}],
+        ['@babel/plugin-proposal-class-properties', {loose: true}]
+    ],
 }
